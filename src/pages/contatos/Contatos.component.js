a870dfaa748a518288747e26ef835f4ed318b2b3
Felipe Sousa <felipzbr@gmail.com>
2018-02-09 02:11:53 -0300

    fixes(general): dropdown/radio/drawer/toolbar_options added

--- src/pages/contatos/Contatos.component.js
+++ src/pages/contatos/Contatos.component.js
@@ -3,9 +3,14 @@
  */
 
 class Contatos extends Polymer.Element {
+    constructor() {
+        super();
+    }
+
     static get is() {
         return 'contatos-page';
     }
+
     static get properties() {
         return {
             // This shouldn't be neccessary, but the Analyzer isn't picking up
@@ -18,9 +23,30 @@ class Contatos extends Polymer.Element {
             selected2:{
                 type: Number,
                 value: 0
+            },
+            // the paper-dropdown value is get using properties, inside de component is setup like {{dropdownValue}}, 
+            // the same name declared on properties here, the value is the value what will be list item value.
+            dropdownValue: {
+                type: String,
+                value: 'Bike'
             }
         };
+    }
 
+    ready() {
+        super.ready();
+        const {
+            paperRadioValue,
+            paperSelect,
+        } = this.$;
+
+        paperRadioValue.addEventListener('click', (e) => {
+            console.log(paperRadioValue.selected); // actually value
+
+            // here you can get the value every when value is changed.
+            // values on child elements are created by `name` property
+            // to get the value of the radio group, you get the property called `selected`, see the example above.
+        })
     }
 }
 
--- src/pages/dashboard/Dashboard.component.js
+++ src/pages/dashboard/Dashboard.component.js
@@ -3,15 +3,31 @@
  */
 
 class Dashboard extends Polymer.Element {
+    constructor() {
+        super();
+    }
+
     static get is() {
         return 'dashboard-page';
     }
+
     static get properties() {
         return {
             // This shouldn't be neccessary, but the Analyzer isn't picking up
             // Polymer.Element#rootPath
             rootPath: String,
         };
+    }
+
+    ready() {
+        super.ready();
+        
+        const {
+            buttonMenu,
+            menuDrawer,
+        } = this.$;
+
+        buttonMenu.addEventListener('click', (e) => { menuDrawer.toggle(); })   
     }
 }
 
