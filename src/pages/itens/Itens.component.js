b885c6a0fad34c2d0ae4aa2f1aeb3ab2ff74b0e0
LAPTOP-9AOTUE6R\ivans <ivanscher@gmail.com>
2018-03-01 11:34:57 -0300

    tela de itens

---
+++ src/pages/itens/Itens.component.js
@@ -0,0 +1,29 @@
+/**
+ * Created by ivans on 30/01/2018.
+ */
+
+class Itens extends Polymer.Element {
+    static get is() {
+        return 'itens-page';
+    }
+    static get properties() {
+        return {
+            // This shouldn't be neccessary, but the Analyzer isn't picking up
+            // Polymer.Element#rootPath
+            rootPath: String,
+            selected: {
+                type: Number,
+                value: 0
+            },
+            selected2:{
+                type: Number,
+                value: 0
+            },
+        };
+    }
+}
+
+window.customElements.define(Itens.is, Itens);
+/**
+ * Created by ivans on 01/03/2018.
+ */
